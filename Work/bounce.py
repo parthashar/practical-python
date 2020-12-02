9fdcf1ece14cf464872c25a35d8b7060fb50d88d
parthashar <parthashar95@gmail.com>
2020-12-02 23:50:16 +0000

    02-dec-2020

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+
+ball_height = int(100)
+
+count = 10
+i = 0
+
+while i < count:
+    i = i + 1
+    ball_height = (ball_height * 3) / 5
+    print (round(ball_height, 4))
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,21 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+months = 0
+
+while principal > 0:
+    while months < 12 :
+        months = months + 1
+        print ("month is ", months)
+        principal = principal * (1+rate/12) - payment - 1000
+        total_paid = total_paid + payment + 1000
+    months = months + 1
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    print ("month is ", months)
+
+print('Total paid', total_paid)
